180d3764633b8ccce854cb5a040279d15f669928
Derek Zak <zak@arizonabay.com>
2019-03-14 16:07:10 -0600

    Fixed destroy all link, added styling, fixed specs, updated version number

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Delayed::Web::Engine.routes.draw do
   root to: 'jobs#index'
 
-  resources :jobs, only: [:destroy, :destroy_all, :index, :show] do
+  resources :jobs, only: [:destroy, :index, :show] do
     put :queue, on: :member
   end
+  post 'destroy_all', to: 'jobs#destroy_all', as: :destroy_all_jobs
 end
